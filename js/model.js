7b3b2edf1e62ebbcca7e0e037746053f0a8e4885
Deniska,VOLKRAFT <volkraft9@gmail.com>
2024-03-01 18:43:19 +0300

    Feat: function for get random couple

--- js/model.js
+++ js/model.js
@@ -1,4 +1,24 @@
 export default class Model {
+    constructor(){
+        this.getRandom(this.questions)
+    }
+    questions = [
+        ['What is the capital of Japan?', 'Tokyo'],
+        ['Who is known as the father of modern physics?', 'Albert Einstein'],
+        ['What is the largest mammal on Earth?', 'Blue whale'],
+        ['What is the chemical symbol for gold?', 'Au'],
+        ['Which planet is known as the Red Planet?', 'Mars'],
+        ['Who wrote the famous novel "1984"?', 'George Orwell'],
+        ['What is the chemical name for table salt?', 'Sodium chloride'],
+        ['What is the process by which plants make their food called?', 'Photosynthesis'],
+        ['Which famous scientist formulated the theory of relativity?', 'Albert Einstein'],
+        ['What is the largest ocean on Earth?', 'Pacific Ocean'],
+    ]
+    
 
-
+    getRandom(questions){
+        const randomIndex = Math.floor(Math.random() * questions.length);
+        const currentCouple = questions[randomIndex];
+        console.log(currentCouple);
+    }
 }
