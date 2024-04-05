fea499d06f32050eadfc0783b014b339d724e4e7
Deniska,VOLKRAFT <volkraft9@gmail.com>
2024-04-05 18:44:27 +0300

    Refactor: refactored controller&created eventHandler func

--- js/controller.js
+++ js/controller.js
@@ -3,24 +3,21 @@ import View from "./view.js";
 
 const model = new Model();
 const view = new View(model.question, model.secretWord, model.keyboard, model.counter);
-view.elements.keyboard.addEventListener('click', (e) => {
-    const keyBtn = e.target.closest('.keyboard__key');
-    if (keyBtn) {        
-        const keyData = keyBtn.dataset.key;
-        model.counterMistake(keyData)
-        view.renderLetter(model.checkKeyInWord(keyData));
-        view.renderMistakeCounter(model.counter)
-        model.checkedPressedKeys(keyData)
-        view.lockButtons(model.checkedKeys)
-    }
-
-})
-window.addEventListener('keyup', (e) => {
-    const keyData = e.key.toLowerCase();
+const eventHandler = (keyData) => {
     model.counterMistake(keyData)
     view.renderLetter(model.checkKeyInWord(keyData));
     view.renderMistakeCounter(model.counter)
     model.checkedPressedKeys(keyData)
     view.lockButtons(model.checkedKeys)
+};
+view.elements.keyboard.addEventListener('click', (e) => {
+    const keyBtn = e.target.closest('.keyboard__key');
+    if (keyBtn) {        
+        const keyData = keyBtn.dataset.key;
+        eventHandler(keyData);
+    }
 })
-
+window.addEventListener('keyup', (e) => {
+    const keyData = e.key.toLowerCase();
+    eventHandler(keyData);
+})
